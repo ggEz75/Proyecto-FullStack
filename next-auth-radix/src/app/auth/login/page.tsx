981f53fd613adafb5b8fc1c59d2d8e568cc8cb1f
ggEz75 <guillegomez575@gmail.com>
2024-12-31 17:11:40 -0300

    Configurando el Proyecto

---
+++ next-auth-radix/src/app/auth/login/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@radix-ui/themes'
+import { BookmarkIcon } from '@radix-ui/react-icons' // importamos el icono que queramos usar
+
+function LoginPage() {
+    return (
+        <div>
+            LoginPage
+            <Button>
+            <BookmarkIcon />
+            Login
+            </Button>
+                
+            
+        </div>
+        
+    )
+}
+
+export default LoginPage
